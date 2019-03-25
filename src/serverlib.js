33aa707f3084c1455f2ad176032177e83de97ba4
Theo van den Heuvel <vdheuvel@heuvelhlt.nl>
2019-03-25 12:50:29 +0100

    add readme and elaborate lib

--- src/serverlib.js
+++ src/serverlib.js
@@ -5,7 +5,6 @@
 "use strict";
 
 module.exports = class Server {
-
   constructor(db_parms, routing) {
     this.db_parms = db_parms;
     this.routing = routing;
@@ -52,16 +51,53 @@ module.exports = class Server {
       }
     });
 
-    function build_crud(from, prefix) {
-      for (let ep in from) {
-        console.log(`setting up crud for ${prefix}${ep}`);
-        app.post(`${prefix}${ep}`, function(req, res) {
+    function build_crud(from, name, prefix) {
+      let flags = {};
+      if (from.crud) {
+        // split from.crud in characters
+        let tail = from.crud.split('');
+        tail.forEach((v) => flags[v] = 1);
+      }
+      if (from.proc) app.get(`${prefix}`, function(req, res) {
+        const obj = req.body;
+        console.log(`setting up proc for ${prefix}`);
+        // console.log(`setting up proc params: ${JSON.stringify(req.params)}`);
+        console.log(`setting up proc query: ${JSON.stringify(req.query)}`);
+        // console.log(`setting up proc route: ${JSON.stringify(req.route)}`);
+        // console.log(`setting up proc res: ${JSON.stringify(res)}`);
+        res.setHeader('Content-Type', 'application/json');
+        // let cmd_tmpl = '' + from.proc;
+        // let cmd = cmd_tmpl.replace(/%/g, name); // TvdH: is this a smart symbol to use for this? Is it safe in SQL?
+        const cmd = from.proc(req.query);
+        console.log(`trying to run "${cmd}" of ${name}`);
+        connection.query(cmd, obj, (err, result) => {
+          if (!err) {
+            console.log(`proc ${cmd} ran`);
+            const info = result;
+            if (info) {
+              res.setHeader('Content-Type', 'application/json')
+              res.status(201).end(JSON.stringify(info));
+              console.log(`proc ${cmd} produced results`);
+            } else {
+              // error, proc failed
+              res.setHeader('Content-Type', 'application/json')
+              res.status(404).end();
+              console.log(`proc ${cmd} did not produce results`);
+            }
+          } else {
+            throw err;
+          }
+        });
+      });
+      if (!from.transparent) { // probably not needed: just skip the crud
+        console.log(`setting up crud for ${prefix}`);
+        if (flags.C) app.post(`${prefix}`, function(req, res) {
           const obj = req.body;
           console.log('trying to post: '+JSON.stringify(obj));
-          connection.query(`INSERT INTO ${ep} SET ?`, obj, (err, result) => {
+          connection.query(`INSERT INTO ${name} SET ?`, obj, (err, result) => {
             if (!err) {
               res.setHeader('Content-Type', 'application/json')
-              connection.query(`SELECT * FROM ${ep} where id=?`, result.insertId, (err, rows) => {
+              connection.query(`SELECT * FROM ${name} where id=?`, result.insertId, (err, rows) => {
                 if (!err) {
                   const info = rows[0];
                   if (info) {
@@ -81,9 +117,9 @@ module.exports = class Server {
             }
           });
         });
-        app.get(`${prefix}${ep}`, function(req, res) {
+        if (flags.R) app.get(`${prefix}`, function(req, res) {
           res.setHeader('Content-Type', 'application/json');
-          connection.query(`SELECT * FROM ${ep}`, (err, objs) => {
+          connection.query(`SELECT * FROM ${name}`, (err, objs) => {
             if (!err) {
               res.end(JSON.stringify(objs));
             } else {
@@ -91,9 +127,9 @@ module.exports = class Server {
             }
           });
         });
-        app.get(`${prefix}${ep}/:id`, function(req, res) {
+        if (flags.r) app.get(`${prefix}/:id`, function(req, res) {
           let id = +req.params.id
-          connection.query(`SELECT * FROM ${ep} where id=?`, id, (err, rows) => {
+          connection.query(`SELECT * FROM ${name} where id=?`, id, (err, rows) => {
             if (!err) {
               console.log('Data received from Db:\n');
               let obj = rows[0];
@@ -109,11 +145,11 @@ module.exports = class Server {
             }
           });
         });
-        app.delete(`${prefix}${ep}/:id`, function(req, res) {
+        if (flags.d) app.delete(`${prefix}/:id`, function(req, res) {
           const id = +req.params.id;
 
           connection.query(
-            `DELETE FROM ${ep} WHERE id = ?`, [id], (err, result) => {
+            `DELETE FROM ${name} WHERE id = ?`, [id], (err, result) => {
               if (!err) {
                 console.log(`Deleted ${result.affectedRows} row(s)`);
                 res.status(204).end();
@@ -124,23 +160,23 @@ module.exports = class Server {
             }
           );
         });
-        if (ep.fields) {
-          app.put(`${prefix}${ep}/:id`, function(req, res) {
+        if (from.fields && flags.u) {
+          app.put(`${prefix}/:id`, function(req, res) {
 
             // First read id from params
             const id = +req.params.id
             const info = req.body;
-            const target = concat(ep.field.map(f=> info[f]), [id]);
+            const target = concat(from.fields.map(f=> info[f]), [id]);
 
             connection.query(
-              `UPDATE ${ep} SET ${ep.fields.map[f=> f+'=?'].join(', ')} Where ID = ?`, target,
+              `UPDATE ${name} SET ${from.fields.map[f=> f+'=?'].join(', ')} Where ID = ?`, target,
               (err, result) => {
                 if (!err) {
                   console.log(`Changed ${result.changedRows} row(s)`);
 
                   // end of the update => send response
                   // execute a query to find the result of the update
-                  connection.query(`SELECT * FROM ${ep} where id=?`, [id], (err, rows) => {
+                  connection.query(`SELECT * FROM ${name} where id=?`, [id], (err, rows) => {
                     if (!err) {
                       console.log('Data received from Db:\n');
 
@@ -167,25 +203,24 @@ module.exports = class Server {
             );
           });
         }
-        if (ep.children) {
-          for (let ch in ep.children) {
-            build_crud(ch, `${prefix}/${ch}`);
-          }
+      }
 
-        }
+      if (from.children) {
+        build_crud_children(from.children, prefix);
       }
     }
 
+    function build_crud_children(children, prefix) {
+      for (let ch in children) {
+        build_crud(children[ch], ch, `${prefix}/${ch}`);
+      }
+    }
 
-
-    build_crud(this.routing, '/api/');
+    build_crud_children(this.routing, '/api');
     // and finally ... run it :-)
     // get the server from the app which runs on port 8081
-    let server = app.listen(8081, function() {
-
+    let server = app.listen(this.db_parms.port, function() {
       console.log("Example app listening at http://%s:%s", server.address().address, server.address().port)
     });
-
   }
-
 }
